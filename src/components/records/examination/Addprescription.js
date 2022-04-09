faadc2805811b45c408003fa8c0bb7567c60dd46
ZeinabNabil <75908709+ZeinabNabil@users.noreply.github.com>
2022-04-09 22:42:06 +0200

    zeinab-add-prescription-add-remove

--- src/components/records/examination/Addprescription.js
+++ src/components/records/examination/Addprescription.js
@@ -4,8 +4,26 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { toast } from "react-toastify";
 import { prescriptionAdd } from "./../../../actions/records/pescriptionaction";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Addprescription = (props) => {
+  const [inputList, setInputList] = useState([
+    { drug: "", dose: "", date: "", note: "" },
+  ]);
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { drug: "", dose: "", date: "", note: "" }]);
+  };
+
   const [form, setForm] = useState({
     drug: "",
     dose: "",
@@ -33,60 +51,83 @@ const Addprescription = (props) => {
     <div className="add-prescription">
       <div className="row prescripton-form">
         <div className="col-lg-12 col-md-12 col-sm-12">
-          <div className="pres-img-container">
+          <div className="pres-container">
             <span className="pres-header">Add Prescription</span>
           </div>
         </div>
         <div className="col-lg-12 col-md-12 col-sm-12">
-          <div className="pres-form">
-            <form className="form-group" onSubmit={onFormSubmit}>
-              <Fields
-                value={form.drug}
-                onChange={onInputChange}
-                name="drug"
-                labelName="drug"
-                type="text"
-                placeholder="Enter drug name"
-              />
-              <Fields
-                value={form.dose}
-                onChange={onInputChange}
-                name="dose"
-                labelName="dose"
-                type="number"
-                placeholder="Enter dose "
-              />
-
-              <Fields
-                value={form.date}
-                onChange={onInputChange}
-                name="date"
-                labelName="Date"
-                type="date"
-              />
-              <div className="row form-container">
-                <div className="formlabel col-lg-3">
-                  <label htmlFor="note">Note:</label>
-                </div>
-                <div className="col-lg-9">
-                  <textarea
-                    value={form.note}
+          {inputList.map((x, i) => {
+            return (
+              <div className="pres-form">
+                <form className="form-group" onSubmit={onFormSubmit}>
+                  <Fields
+                    value={form.drug}
                     onChange={onInputChange}
-                    name="note"
-                    id="date"
-                    className="form-control"
-                    rows={3}
-                    placeholder="Any notes !... "
-                  ></textarea>
-                </div>
+                    name="drug"
+                    labelName="Drug"
+                    type="text"
+                    placeholder="Enter drug name"
+                  />
+                  <Fields
+                    value={form.dose}
+                    onChange={onInputChange}
+                    name="dose"
+                    labelName="Dose"
+                    type="number"
+                    placeholder="Enter dose "
+                  />
+                  <Fields
+                    value={form.date}
+                    onChange={onInputChange}
+                    name="date"
+                    labelName="Date"
+                    type="date"
+                  />
+                  <div className="row form-container">
+                    <div className="formlabel col-lg-3">
+                      <label htmlFor="note">Note:</label>
+                    </div>
+                    <div className="col-lg-9">
+                      <textarea
+                        value={form.note}
+                        onChange={onInputChange}
+                        name="note"
+                        id="date"
+                        className="form-control"
+                        rows={3}
+                        placeholder="Any notes !... "
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="add-btns">
+                      <div className="col-lg-12 col-md-12 col-sm-12 add-remove-pres">
+                        {inputList.length !== 1 && (
+                          <button
+                            className="btn btn-primary mb-2"
+                            onClick={() => handleRemoveClick(i)}
+                          >
+                            <FontAwesomeIcon icon={faTrash} />
+                          </button>
+                        )}
+                        {inputList.length - 1 === i && (
+                          <button
+                            onClick={handleAddClick}
+                            className="btn btn-primary mb-2"
+                          >
+                            <FontAwesomeIcon icon={faPlus} />
+                          </button>
+                        )}
+                      </div>
+                      <div className="col-lg-12 col-md-12 col-sm-12 submit-pres">
+                        <button className="btn btn-primary mb-2">Add</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
               </div>
-              <div className="row">
-                <div className="col-lg-12 add-btn">
-                  <button className="btn btn-primary mb-2">Add</button>
-                </div>
-              </div>
-            </form>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>

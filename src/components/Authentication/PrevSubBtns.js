4909b35a0cd8b0e70727ad0ec4c3a7c54a060636
ZeinabNabil <75908709+ZeinabNabil@users.noreply.github.com>
2022-05-31 23:13:55 +0200

    zeinab-login-register-responsive

--- src/components/Authentication/PrevSubBtns.js
+++ src/components/Authentication/PrevSubBtns.js
@@ -3,23 +3,19 @@ import style from "../../Css/Authentication/Auth.module.css";
 
 const PrevSubBtns = (props) => {
   return (
-    <div className="btns-container-register-steps mt-8">
-      <div className="row">
-        <div className="col-lg-6 col-md-6 col-sm-6">
-          <div className="input-group mb-3">
-            <div
-              onClick={props.onClick}
-              className={`btn ${style.btns_auth_steps}`}
-            >
-              PREV
-            </div>
-          </div>
+    <div
+      className={`${style.btns_container_register_steps} btns-container-register-steps mt-8`}
+    >
+      <div className={style.prev_submit_container}>
+        <div onClick={props.onClick} className={`btn ${style.btns_auth_steps}`}>
+          PREV
         </div>
-        <div className="col-lg-6 col-md-6 col-sm-6">
-          <div className="input-group mb-3" style={{ justifyContent: "right" }}>
-            <button className={`btn ${style.btns_auth_steps}`}>Submit</button>
-          </div>
-        </div>
+      </div>
+      <div
+        className={style.prev_submit_container}
+        style={{ justifyContent: "right" }}
+      >
+        <button className={`btn ${style.btns_auth_steps}`}>Submit</button>
       </div>
     </div>
   );
